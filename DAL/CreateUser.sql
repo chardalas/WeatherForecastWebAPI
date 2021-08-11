5d0a88ee646f8d97a703b569822a4c75d710e5ce
chardalas <emmhard@gmail.com>
2021-08-12 01:41:02 +0200

    Indentention fix

--- DAL/CreateUser.sql
+++ DAL/CreateUser.sql
@@ -6,8 +6,8 @@
 CREATE PROCEDURE CreateUser
 	-- Add the parameters for the stored procedure here
 	@firstName NVARCHAR(max), 
-    @lastName NVARCHAR(max),	
-    @email NVARCHAR(max),
+	@lastName NVARCHAR(max),	
+	@email NVARCHAR(max),
 	@password NVARCHAR(max)
 AS
 BEGIN
@@ -15,8 +15,8 @@ BEGIN
 	-- interfering with SELECT statements.
 	SET NOCOUNT ON;
 
-    -- Insert statements for procedure here
+	-- Insert statements for procedure here
 	 INSERT INTO dbo.[User] (FirstName, LastName, Email, Password)
-        VALUES(@firstName, @lastName, @email, HASHBYTES('SHA2_512', @password))
+		VALUES(@firstName, @lastName, @email, HASHBYTES('SHA2_512', @password))
 
 END
