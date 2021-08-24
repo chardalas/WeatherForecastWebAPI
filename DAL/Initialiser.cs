2ee10f15c0996fb488d66fe4886b48aa3069b79b
chardalas <emmhard@gmail.com>
2021-08-24 08:45:48 +0200

    Rename name

--- DAL/Initialiser.cs
+++ DAL/Initialiser.cs
@@ -17,7 +17,7 @@ namespace WeatherForecast.DAL
                 new User{FirstName ="Lois",     LastName="Jacker",  Email = "jackerl@weatherforecast.org",   Password = new byte[] { 0x02,0x03,0x04,0x01 }},
                 new User{FirstName ="Paul",     LastName="Mantra",  Email = "mantrap@weatherforecast.org",   Password = new byte[] { 0x03,0x04,0x02,0x01 }},
                 new User{FirstName ="Christi",  LastName="Mayher",  Email = "mayherc@weatherforecast.org",   Password = new byte[] { 0x049,0x7A,0x6A,0x5E }},
-                new User{FirstName ="Roben",    LastName="Rabap",   Email = "rabapr@weatherforecast.org",    Password = new byte[] { 0x01,0x02,0x03,0x04 }}
+                new User{FirstName ="Roben1",    LastName="Rabap",   Email = "rabapr@weatherforecast.org",    Password = new byte[] { 0x01,0x02,0x03,0x04 }}
             };
 
             users.ForEach(s => context.Users.Add(s));
